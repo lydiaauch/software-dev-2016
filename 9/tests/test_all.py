79843e824ddddeead3b83d1bead562ed5e76ff59
Alex <jo.al@husky.neu.edu>
2016-03-14 18:40:15 -0400

    Updating to latest

---
+++ 9/tests/test_all.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import subprocess
+import string
+
+evolution_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                "..%s" % os.sep)
+sys.path.append(evolution_path)
+
+if __name__ == '__main__':
+    files = os.listdir(os.getcwd())
+    for i in sorted(files):
+        if i.endswith("in.json"):
+            print("--------  " + i + "  --------")
+            file = open(i)
+            p1 = subprocess.Popen(["./xstep", ""], stdin=file, stdout=subprocess.PIPE, cwd=os.path.relpath(".."))
+            result = p1.stdout.read()
+            output,err = p1.communicate()
+            outfile_name = i.replace("in.json", "out.json")
+            expected = open(outfile_name).read()
+            if (result.translate(None, string.whitespace) == expected.translate(None, string.whitespace)):
+                print("The test passed! \n")
+            else:
+                print("The test FAILED.\n")
+                print("Expected:\n" + expected)
+                print("Actual:\n" + result)
